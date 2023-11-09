67bb0d622e55848988e41e7b00028366a7e838f0
salem.domani <salem.domani@gmail.com>
2023-11-09 13:26:27 +0300

    fix: super tab auto complete

--- lua/plugins/nvimcmp.lua
+++ lua/plugins/nvimcmp.lua
@@ -1,51 +1,65 @@
 return {
-  "hrsh7th/nvim-cmp",
-  dependencies = {
-    "hrsh7th/cmp-emoji",
+  {
+    "L3MON4D3/LuaSnip",
+    keys = function()
+      return {}
+    end,
   },
-  ---@param opts cmp.ConfigSchema
-  opts = function(_, opts)
-    local luasnip = require("luasnip")
-    local cmp = require("cmp")
-
-    opts.mapping = vim.tbl_extend("force", opts.mapping, {
-      ["<Tab>"] = cmp.mapping(function(fallback)
-        print("tabbed")
-        -- This little snippet will confirm with tab, and if no entry is selected, will confirm the first item
-        if cmp.visible() then
-          local entry = cmp.get_selected_entry()
-          if not entry then
-            cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-          else
-            cmp.confirm()
-          end
-        else
-          fallback()
-        end
-      end, { "i", "s", "c" }),
-      ["<S-Tab>"] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.select_prev_item()
-        elseif luasnip.jumpable(-1) then
-          luasnip.jump(-1)
-        else
-          fallback()
-        end
-      end, { "i", "s" }),
-    })
-  end,
-  ---@param opts cmp.ConfigSchema
-  config = function(_, opts)
-    opts.enabled = function()
-      -- disable completion in comments
-      local context = require("cmp.config.context")
-      -- keep command mode completion enabled when cursor is in a comment
-      if vim.api.nvim_get_mode().mode == "c" then
-        return true
-      else
-        return not context.in_treesitter_capture("comment") and not context.in_syntax_group("Comment")
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-emoji",
+    },
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      local has_words_before = function()
+        unpack = unpack or table.unpack
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
       end
-    end
-    require("cmp").setup(opts)
-  end,
+
+      local luasnip = require("luasnip")
+      local cmp = require("cmp")
+
+      opts.mapping = vim.tbl_extend("force", opts.mapping, {
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
+            cmp.confirm({ select = true })
+          -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+          -- this way you will only jump inside the snippet region
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          elseif has_words_before() then
+            cmp.complete()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      })
+    end,
+    ---@param opts cmp.ConfigSchema
+    config = function(_, opts)
+      opts.enabled = function()
+        -- disable completion in comments
+        local context = require("cmp.config.context")
+        -- keep command mode completion enabled when cursor is in a comment
+        if vim.api.nvim_get_mode().mode == "c" then
+          return true
+        else
+          return not context.in_treesitter_capture("comment") and not context.in_syntax_group("Comment")
+        end
+      end
+      require("cmp").setup(opts)
+    end,
+  },
 }
