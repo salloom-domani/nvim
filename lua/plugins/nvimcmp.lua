4b304f2391d41295e7bc9566f30c0c4e62e2ff30
Salem Domani <salem.domani@gmail.com>
2024-05-23 23:19:05 +0300

    fix: all annoying auto-cmp bugs
    No more global snippets in telescope and neo-tree and no more empty
    suggestions inside JS objects.
    I was digging my own hole with snippets I got from the internet.
    Maybe I should stop copying and pasting into my config without knowing
    what it does.

--- lua/plugins/nvimcmp.lua
+++ lua/plugins/nvimcmp.lua
@@ -1,11 +1,5 @@
 return {
   {
-    "L3MON4D3/LuaSnip",
-    keys = function()
-      return {}
-    end,
-  },
-  {
     "hrsh7th/nvim-cmp",
     dependencies = {
       "hrsh7th/cmp-emoji",
@@ -46,20 +40,6 @@ return {
           end
         end, { "i", "s" }),
       })
-    end,
-    ---@param opts cmp.ConfigSchema
-    config = function(_, opts)
-      opts.enabled = function()
-        -- disable completion in comments
-        local context = require("cmp.config.context")
-        -- keep command mode completion enabled when cursor is in a comment
-        if vim.api.nvim_get_mode().mode == "c" then
-          return true
-        else
-          return not context.in_treesitter_capture("comment") and not context.in_syntax_group("Comment")
-        end
-      end
-      require("cmp").setup(opts)
     end,
   },
 }
