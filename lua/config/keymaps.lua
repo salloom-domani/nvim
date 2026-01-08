5da6ae1ea34e319c235b563c2d96ac558230a117
Salem Domani <salem.domani@gmail.com>
2026-01-08 16:29:51 +0300

    feat: keymaps housekeeping

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,11 +2,17 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+vim.keymap.set("i", "jj", "<esc>", { desc = "Esc to exit insert mode" })
+
 -- Additional yank operations
-vim.keymap.set("n", "<leader>fy", "ggyG", { desc = "[F]ile [Y]ank " })
-vim.keymap.set("n", "<leader>fv", "ggVG", { desc = "[F]ile [V] select " })
+vim.keymap.set("n", "<leader>fy", "<CMD>%yank<CR>", { desc = "[F]ile [Y]ank " })
+vim.keymap.set("n", "<leader>fv", "ggVG", { desc = "[F]ile [V]elect " })
 vim.keymap.set("n", "<leader>y", '"+y', { desc = "Yank to clipboard" })
 vim.keymap.set("v", "<leader>y", '"+y', { desc = "Yank selected to clipboard" })
+
+vim.keymap.set("n", "<C-q>", function()
+  Snacks.bufdelete()
+end, { desc = "[Q]uit buffer" })
 
 -- Dadbod UI
 vim.keymap.set("n", "<leader>db", "<CMD>DBUIToggle<CR>", { desc = "Dadbod UI" })
