e358bcbe13a1cc8f98329fd82c5e059285cead72
Salem Domani <salem.domani@gmail.com>
2026-01-08 16:30:08 +0300

    chore: disable snacks animations

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,11 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- If no prettier config file is found, the formatter will not be used
+-- vim.g.lazyvim_prettier_needs_config = true
+
+vim.g.snacks_animate = false
+
 local opt = vim.opt
 
 -- Ignore compiled files
